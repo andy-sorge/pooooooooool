c051f322babcc53ce94aae976dcd393032589167
GoldenGuy1000 <GoldenGuy1000Contact@gmail.com>
2026-05-02 00:19:28 -0700

    hella epsilon

--- include/Game.hpp
+++ include/Game.hpp
@@ -132,7 +132,7 @@ private:
                 auto state = state_.lock();
                 state->turn = PlayerTurn::Aiming;
                 for (const Ball& ball : state->balls) {
-                    if (ball.vel.magnitude() > 0.1) {
+                    if (ball.vel.magnitude() > 50.0) {
                         state->turn = PlayerTurn::Physics;
                         break;
                     }
