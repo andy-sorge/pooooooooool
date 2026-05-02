d748cfde4f6304498fb0050ce613c50210afe25c
GoldenGuy1000 <GoldenGuy1000Contact@gmail.com>
2026-05-01 23:59:38 -0700

    stick dir & epsilon reduced

--- include/Game.hpp
+++ include/Game.hpp
@@ -132,7 +132,7 @@ private:
                 auto state = state_.lock();
                 state->turn = PlayerTurn::Aiming;
                 for (const Ball& ball : state->balls) {
-                    if (ball.vel.magnitude() > 0.004) {
+                    if (ball.vel.magnitude() > 0.1) {
                         state->turn = PlayerTurn::Physics;
                         break;
                     }
